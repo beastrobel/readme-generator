f5e94be18d881fb3d49a542babe7ad188a9982c2
beastrobel <beatrizstrobel@proton.me>
2023-10-08 19:53:07 -0400

    Installed inquirer and added prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
+//Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'Input project title:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Input project description:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Input table of contents:',
+        name: 'toc',
+    },
+    {
+        type: 'input',
+        message: 'Input installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Input usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Input license:',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Input contributors:',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Input tests:',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Input questions:',
+        name: 'questions',
+    },
+  ])
+  .then((response) =>
+    console.log(response)
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
